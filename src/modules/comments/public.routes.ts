d2e8ac19921a2ca87f572a387a9a46ede0360a19
Victor Ibironke <victoribironke2019@gmail.com>
2026-06-10 22:45:57 +0100

    feat: add route for retrieving comment replies by parent ID

--- src/modules/comments/public.routes.ts
+++ src/modules/comments/public.routes.ts
@@ -7,5 +7,6 @@ const controller = new CommentsController();
 // Public routes mounted at /api/v2/comments
 router.post('/:slug', controller.addComment);
 router.get('/:slug', controller.getComments);
+router.get('/:slug/replies/:parentId', controller.getReplies);
 
 export default router;
